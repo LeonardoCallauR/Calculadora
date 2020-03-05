b7700a21a3917c2a0a0eeb271e8af8516fe9ec80
leo_cr_m@hotmail.com <leo_cr_m@hotmail.com>
2020-03-05 19:57:18 -0400

    Se agrago el Unitest

---
+++ WebApiCalculadora/UTCalculadora/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UTCalculadora
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
