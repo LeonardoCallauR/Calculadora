14c8127bb77c11979e07e81338e3a5b9a79677c6
leo_cr_m@hotmail.com <leo_cr_m@hotmail.com>
2020-03-07 10:13:00 -0400

    Primer Unitest con Add

--- WebApiCalculadora/UTCalculadora/UnitTest1.cs
+++ WebApiCalculadora/UTCalculadora/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApiCalculadora.Controllers;
 
 namespace UTCalculadora
 {
@@ -6,8 +7,17 @@ namespace UTCalculadora
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddGet1()
         {
+            //Arrange
+            CalculatorController calc = new CalculatorController();
+            int NumA = 3;
+            int NumB = 4;
+            int NumC = 7;
+            //Act
+            int result = calc.Add(NumA, NumB);
+            //Aaaee
+            Assert.AreEqual(NumC, result);
         }
     }
 }
